43be8a08405160e477b74fc731575bd3eedcf947
Krzych <krzych.pierzchala@gmail.com>
2017-09-20 12:57:49 +0200

    Added some comments into scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+//scripts.js file
+
 var name = prompt("Enter your name");
-alert("Hello "+ name);
+//alert("Hello "+ name);
 console.log("Hello " + name);
+
+/*Bardzo proste te zadania.
+Kiedy w końcu będzie coś ambitniejszego? :) */
+
+//To chyba było najprostsze zadanie na Kodilli
